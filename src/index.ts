dfac3d53ee1e9c3fd2a2ba94a97f0629b5e09598
Jaime <jaimeloeuf@gmail.com>
2022-08-18 15:27:52 +0800

    Add named export from entry point:     So that users can import the component explicitly from the module     and use inside their SFCs when they do not want to register the     loader component globally.

--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,8 @@ export default {
     app.component("Loader", Loader);
   },
 };
+
+// Named export for users to import the component from this entry point file,
+// So that they can explicitly define the component import when using inside their SFCs,
+// instead of injecting this as a global component available to the entire app.
+export { Loader };
